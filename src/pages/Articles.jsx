a84c4d77c5ce656a7b5b5ab3c1c99a3b52a2025b
chanuka randitha <chanukaranditha99@gmail.com>
2026-07-14 23:36:18 +0530

    create articles

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import ArticleCard from '../components/ArticleCard'
+
 
 const Articles = () => {
   return (
-    <div>
-      Articals
-    </div>
+    <section className="section container">
+      <div className="page-heading">
+        <span>Football content</span>
+        <h1>Latest Articles</h1>
+        <p>
+          Explore football stories, player profiles, match analysis and
+          historical moments.
+        </p>
+      </div>
+
+      <div className="article-grid">
+        {articles.map((article) => (
+          <ArticleCard key={article.id} article={article} />
+        ))}
+      </div>
+    </section>
   )
 }
 
