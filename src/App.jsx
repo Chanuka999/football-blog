9f1c0a2ef04589731e0cad71a448db88d54a4eb3
chanuka randitha <chanukaranditha99@gmail.com>
2026-07-14 23:34:55 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Articles from './pages/Articles'
+import ArticleDetails from './pages/ArticleDetails'
+import About from './pages/About'
+import Footer from './components/Footer'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
